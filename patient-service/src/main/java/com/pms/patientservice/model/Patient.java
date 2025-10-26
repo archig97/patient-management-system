96c556d6c13a46f1620bfd572eb6c60873a1842d
archig97 <archismitaghosh@yahoo.com>
2025-10-26 18:26:04 -0400

    generated getters and setters for Patient

--- patient-service/src/main/java/com/pms/patientservice/model/Patient.java
+++ patient-service/src/main/java/com/pms/patientservice/model/Patient.java
@@ -14,7 +14,7 @@ public class Patient {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
 
-    @NotNull
+     @NotNull
     private String name;
 
     @NotNull
@@ -30,4 +30,54 @@ public class Patient {
 
     @NotNull
     private LocalDate registeredDate;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public void setId(UUID id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public LocalDate getBirthDate() {
+        return birthDate;
+    }
+
+    public void setBirthDate(LocalDate birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public LocalDate getRegisteredDate() {
+        return registeredDate;
+    }
+
+    public void setRegisteredDate(LocalDate registeredDate) {
+        this.registeredDate = registeredDate;
+    }
+
+
 }
