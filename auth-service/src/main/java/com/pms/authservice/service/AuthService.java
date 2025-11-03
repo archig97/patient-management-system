3a9a7aa34037076dc15d1bb6fb5d450f58b6fb57
archig97 <archismitaghosh@yahoo.com>
2025-11-03 09:45:50 -0500

    basic Security Config created

--- auth-service/src/main/java/com/pms/authservice/service/AuthService.java
+++ auth-service/src/main/java/com/pms/authservice/service/AuthService.java
@@ -4,18 +4,29 @@ import java.util.Optional;
 
 import com.pms.authservice.dto.LoginRequestDTO;
 import com.pms.authservice.model.User;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
 public class AuthService {
 
     private final UserService userService;
+    private final PasswordEncoder passwordEncoder;
 
-    public AuthService(UserService userService) {
+    public AuthService(UserService userService, PasswordEncoder passwordEncoder) {
         this.userService = userService;
+        this.passwordEncoder = passwordEncoder;
     }
-
+    //if password matches, it is encodedgit add .
+    git commit -m ""
     public Optional<String> authenticate(LoginRequestDTO loginRequestDTO) {
-        Optional<User> user=userService.findByEmail(loginRequestDTO.getEmail());
+        Optional<User> user=userService
+                .findByEmail(loginRequestDTO.getEmail())
+                .filter(u -> passwordEncoder.matches(loginRequestDTO.getPassword(),u.getPassword()))
+                .map(u -> jwtUtil.generateToken(u.getEmail(),u.getRole()));
+
+        return token;
+
+
     }
 }
